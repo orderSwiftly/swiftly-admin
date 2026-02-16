1256af03c6b466bda8ebc8ccdc1614230fa94ded
VictorDev <ememevictor08@gmail.com>
2026-02-16 21:21:41 +0100

    user and order view ui done

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,9 +8,14 @@ export default function DashboardLayout({
   readonly children: React.ReactNode;
 }) {
   return (
-    <div className="flex bg-gray-50 min-h-screen">
-      <Sidebar />
-      <main className="flex-1 md:ml-64">{children}</main>
-    </div>
+    <body>
+      <header>
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital@0;1&display=swap" rel="stylesheet" />
+      </header>
+      <div className="flex bg-gray-50 min-h-screen">
+        <Sidebar />
+        <main className="flex-1 md:ml-64 pry-ff">{children}</main>
+      </div>
+    </body>
   );
 }
